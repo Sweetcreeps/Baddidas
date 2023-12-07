1b3a5cdf5e45cb5fb5fc85c43500943fc5749178
Sweetcreeps <135039113+Sweetcreeps@users.noreply.github.com>
2023-12-07 09:48:32 +0000

    Updates

---
+++ precache-manifest.91cb813b5a035afbc64cd342eb5329c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7559ca09ac54ed587a6bdce03abd81",
+    "url": "/Baddidas/index.html"
+  },
+  {
+    "revision": "5e86730ff8d014383325",
+    "url": "/Baddidas/static/css/main.1c10791c.chunk.css"
+  },
+  {
+    "revision": "fc81d6e1951153aeb2ef",
+    "url": "/Baddidas/static/js/2.8b3b8f9f.chunk.js"
+  },
+  {
+    "revision": "9ce0a34b6e7c072e8ae95df9bcbfbe1b",
+    "url": "/Baddidas/static/js/2.8b3b8f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e86730ff8d014383325",
+    "url": "/Baddidas/static/js/main.1d82c582.chunk.js"
+  },
+  {
+    "revision": "06647cb336424b364c60",
+    "url": "/Baddidas/static/js/runtime-main.63a6e544.js"
+  }
+]);
